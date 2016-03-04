fec66965c7ff61f6834ba2c4d1779f34a8e545b9
João Simões <nogravity00@outlook.com>
2016-03-04 10:14:23 +0000

    Added SimpleSOAPClient project

---
+++ SimpleSOAPClient/src/SimpleSOAPClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleSOAPClient")]
+[assembly: AssemblyDescription("Lightweight SOAP client for invoking HTTP SOAP endpoints")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Net.JoaoSimoes")]
+[assembly: AssemblyProduct("SimpleSOAPClient")]
+[assembly: AssemblyCopyright("Copyright © 2016 João Simões")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+#if !(PORTABLE40 || PORTABLE)
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9a03d5df-2a75-48c4-aa1a-a52774bd4f14")]
+
+#endif
+
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
