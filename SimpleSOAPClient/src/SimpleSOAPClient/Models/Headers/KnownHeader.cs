6db184d4d4bbf5b04dc51a77ee987db78e6325f5
João Simões <nogravity00@outlook.com>
2016-03-07 17:54:39 +0000

    Changed Id generation

--- SimpleSOAPClient/src/SimpleSOAPClient/Models/Headers/KnownHeader.cs
+++ SimpleSOAPClient/src/SimpleSOAPClient/Models/Headers/KnownHeader.cs
@@ -34,15 +34,19 @@
                 public static UsernameTokenAndPasswordTextSoapHeader UsernameTokenAndPasswordText(
                     string username, string password, bool mustUnderstand = true)
                 {
+                    var randomId = Guid.NewGuid().ToString("N");
+
                     return new UsernameTokenAndPasswordTextSoapHeader
                     {
                         Timestamp = new Timestamp
                         {
+                            Id = string.Concat("_TS", randomId),
                             Created = DateTime.Now,
                             Expires = DateTime.Now.AddMinutes(15)
                         },
                         UsernameToken = new UsernameTokenWithPasswordText
                         {
+                            Id = string.Concat("_UT", randomId),
                             Username = username,
                             Password = new UsernameTokenPasswordText
                             {
