1359f97e4eaa4d50e34348bc3501bcbab92ab823
João Simões <nogravity00@outlook.com>
2016-03-08 15:49:43 +0000

    Added support for DNX Core 5, NET Core 45, DotNet Release 1.0.0-rc1

--- SimpleSOAPClient/src/SimpleSOAPClient/Properties/AssemblyInfo.cs
+++ SimpleSOAPClient/src/SimpleSOAPClient/Properties/AssemblyInfo.cs
@@ -46,4 +46,4 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha2")]
+[assembly: AssemblyInformationalVersion("1.0.0-rc1")]
