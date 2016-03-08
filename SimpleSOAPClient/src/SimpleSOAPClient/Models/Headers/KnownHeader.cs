1fb3b05f08b643e19353bfd3551b36edf582d3fd
João Simões <nogravity00@outlook.com>
2016-03-08 11:02:34 +0000

    Missing documentations

--- SimpleSOAPClient/src/SimpleSOAPClient/Models/Headers/KnownHeader.cs
+++ SimpleSOAPClient/src/SimpleSOAPClient/Models/Headers/KnownHeader.cs
@@ -27,10 +27,22 @@ namespace SimpleSOAPClient.Models.Headers
     using Microsoft;
     using Oasis.Security;
 
+    /// <summary>
+    /// Class with known <see cref="SoapHeader"/> builder methods.
+    /// </summary>
     public static class KnownHeader
     {
+        /// <summary>
+        /// Class with Microsoft specific <see cref="SoapHeader"/> builder methods.
+        /// </summary>
         public static class Microsoft
         {
+            /// <summary>
+            /// Creates a new Microsoft Action SOAP Header.
+            /// </summary>
+            /// <param name="action">The action for the header</param>
+            /// <param name="mustUnderstand">Does the server must understand the header?</param>
+            /// <returns>The new <see cref="ActionSoapHeader"/></returns>
             public static ActionSoapHeader Action(string action, bool mustUnderstand = true)
             {
                 return new ActionSoapHeader
@@ -40,6 +52,12 @@ namespace SimpleSOAPClient.Models.Headers
                 };
             }
 
+            /// <summary>
+            /// Creates a new Microsoft To SOAP Header.
+            /// </summary>
+            /// <param name="to">The action for the header</param>
+            /// <param name="mustUnderstand">Does the server must understand the header?</param>
+            /// <returns>The new <see cref="ToSoapHeader"/></returns>
             public static ToSoapHeader To(string to, bool mustUnderstand = true)
             {
                 return new ToSoapHeader
@@ -50,10 +68,23 @@ namespace SimpleSOAPClient.Models.Headers
             }
         }
 
+        /// <summary>
+        /// Class with Oasis specific <see cref="SoapHeader"/> builder methods.
+        /// </summary>
         public static class Oasis
         {
+            /// <summary>
+            /// Class with Oasis Security specific <see cref="SoapHeader"/> builder methods.
+            /// </summary>
             public static class Security
             {
+                /// <summary>
+                /// Creates a new Oasis Security Username Token with password text SOAP header.
+                /// </summary>
+                /// <param name="username">The username</param>
+                /// <param name="password">The password</param>
+                /// <param name="mustUnderstand">Does the server must understand the header?</param>
+                /// <returns>The new <see cref="UsernameTokenAndPasswordTextSoapHeader"/></returns>
                 public static UsernameTokenAndPasswordTextSoapHeader UsernameTokenAndPasswordText(
                     string username, string password, bool mustUnderstand = true)
                 {
