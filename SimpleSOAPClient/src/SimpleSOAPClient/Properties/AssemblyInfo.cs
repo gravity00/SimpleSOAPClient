dba39b0e022c2702e9ba968a0e1d35e67f8a1e9d
João Simões <nogravity00@outlook.com>
2016-04-15 16:10:14 +0100

    Release 2.0.0-alpha01

--- SimpleSOAPClient/src/SimpleSOAPClient/Properties/AssemblyInfo.cs
+++ SimpleSOAPClient/src/SimpleSOAPClient/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Runtime.InteropServices;
 
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyInformationalVersion("1.1.0")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0-alpha01")]
