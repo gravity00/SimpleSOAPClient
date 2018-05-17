3560ee369924032e8cd1b95938e5d502b2247bc5
Jason Harmon <jdharmon@sentara.com>
2018-05-17 16:59:05 -0400

    Added support for DataContractSerializer.

---
+++ tests/SimpleSOAPClient.Tests/XmlModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Serialization;
+
+namespace SimpleSOAPClient.Tests
+{
+    [XmlRoot(Namespace = "urn:simplesoapclient:test")]
+    public class XmlModel
+    {
+        [XmlElement(Order = 0)]
+        public string String { get; set; }
+
+        [XmlElement(Order = 1)]
+        public int Int { get; set; }
+
+        [XmlElement(Order = 2)]
+        public bool Bool { get; set; }
+
+        [XmlArray(Order = 3)]
+        public string[] Array { get; set; }
+    }
+}
